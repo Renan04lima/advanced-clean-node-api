d24bf61a3d4030e0872b8de64ddd962a999cf1c6
renan04lima <renan04lima@outlook.com>
2024-01-02 23:04:39 -0300

    test: ensure AxiosHttpClient rethrows if axios.get throws

--- tests/infra/http/client.spec.ts
+++ tests/infra/http/client.spec.ts
@@ -44,5 +44,16 @@ describe('AxiosHttpClient', () => {
 
             expect(data).toEqual('any_data')
         })
+
+        it('should rethrow if get throws', async () => {
+            fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+            const promise = sut.get({
+                url,
+                params
+            })
+
+            await expect(promise).rejects.toThrow(new Error('http_error'))
+        })
     })
 })

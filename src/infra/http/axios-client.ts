f3cd3f8ad027660f03c29597c557aff2f84536cb
renan04lima <renan04lima@outlook.com>
2024-01-03 08:11:45 -0300

    make AxiosHttpClient implements HttpClient

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -2,8 +2,8 @@ import { HttpClient } from "./client";
 
 import axios from "axios";
 
-export class AxiosHttpClient {
-    async get({ url, params }: HttpClient.Input): Promise<void> {
+export class AxiosHttpClient implements HttpClient {
+    async get<T = any>({ url, params }: HttpClient.Input): Promise<T> {
         const response = await axios.get(url, { params })
         return response.data
     }

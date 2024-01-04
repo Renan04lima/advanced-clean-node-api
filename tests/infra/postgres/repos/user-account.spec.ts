bfb13de7c3283fa69da94facf5a0be5c6c4a46cf
renan04lima <renan04lima@outlook.com>
2024-01-03 22:18:05 -0300

    refactor: share sut creation with save

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -6,30 +6,29 @@ import { Repository } from 'typeorm'
 import { makeFakeDb } from '../mocks/connection';
 
 describe('UserAccountRepo', () => {
+    let db: IMemoryDb
+    let sut: PgUserAccountRepository
+    let connection: any
+    let pgUserRepo: Repository<PgUser>
+    let backup: IBackup
+
+    beforeAll(async () => {
+        ({ db, connection } = await makeFakeDb([PgUser]))
+        backup = db.backup();
+
+        pgUserRepo = connection.getRepository(PgUser)
+    })
+
+    beforeEach(() => {
+        backup.restore()
+        sut = new PgUserAccountRepository(pgUserRepo)
+    })
+
+    afterAll(async () => {
+        await connection.close()
+    })
+
     describe('load', () => {
-        let db: IMemoryDb
-        let sut: PgUserAccountRepository
-        let connection: any
-        let pgUserRepo: Repository<PgUser>
-        let backup: IBackup
-
-        beforeAll(async () => {
-            ({ db, connection } = await makeFakeDb([PgUser]))
-            backup = db.backup();
-
-            pgUserRepo = connection.getRepository(PgUser)
-        })
-
-        beforeEach(() => {
-            backup.restore()
-            sut = new PgUserAccountRepository(pgUserRepo)
-        })
-
-        afterAll(async () => {
-            await connection.close()
-        })
-
-
         it('should return an account if email exists', async () => {
             const sut = new PgUserAccountRepository(pgUserRepo)
             await pgUserRepo.save({ email: 'any_email' })
@@ -42,8 +41,6 @@ describe('UserAccountRepo', () => {
         })
 
         it('should return undefined if email not exists', async () => {
-            const sut = new PgUserAccountRepository(pgUserRepo)
-
             const account = await sut.load({ email: 'any_email' })
 
             expect(account).toBeUndefined()

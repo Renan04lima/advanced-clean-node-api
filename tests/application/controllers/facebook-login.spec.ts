e51b1731005cbe8f17f274a54674c3bd95062f7d
renan04lima <renan04lima@outlook.com>
2024-01-03 23:26:08 -0300

    test: ensure FacebookLoginController return 401 if invalid credentials are provided

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,19 +1,29 @@
+import { AuthenticationError } from "@/domain/errors/authentication"
 import { FacebookAuthentication } from "@/domain/use-cases/facebook-authentication"
 import { MockProxy, mock } from "jest-mock-extended"
+
+type HttpResponse = {
+    statusCode: number
+    data: any
+}
 
 class FacebookLoginController {
     constructor(
         private readonly facebookAuth: FacebookAuthentication
     ) { }
-    
-    handle(httpRequest: any): any {
+
+    async handle(httpRequest: any): Promise<any> {
         if (!httpRequest.token) {
             return {
                 statusCode: 400,
                 data: new Error('The field token is required')
             }
         }
-        this.facebookAuth.execute({ token: httpRequest.token })
+        const result = await this.facebookAuth.execute({ token: httpRequest.token })
+        return {
+            statusCode: 401,
+            data: result
+        }
     }
 }
 
@@ -26,8 +36,8 @@ describe('FacebookLoginController', () => {
         sut = new FacebookLoginController(facebookAuth)
     })
 
-    it('should return 400 if token is empty', () => {
-        const httpResponse = sut.handle({ token: '' })
+    it('should return 400 if token is empty', async () => {
+        const httpResponse = await sut.handle({ token: '' })
 
         expect(httpResponse).toEqual({
             statusCode: 400,
@@ -35,8 +45,8 @@ describe('FacebookLoginController', () => {
         })
     })
 
-    it('should return 400 if token is null', () => {
-        const httpResponse = sut.handle({ token: null })
+    it('should return 400 if token is null', async () => {
+        const httpResponse = await sut.handle({ token: null })
 
         expect(httpResponse).toEqual({
             statusCode: 400,
@@ -44,8 +54,8 @@ describe('FacebookLoginController', () => {
         })
     })
 
-    it('should return 400 if token is undefined', () => {
-        const httpResponse = sut.handle({ token: undefined })
+    it('should return 400 if token is undefined', async () => {
+        const httpResponse = await sut.handle({ token: undefined })
 
         expect(httpResponse).toEqual({
             statusCode: 400,
@@ -58,5 +68,16 @@ describe('FacebookLoginController', () => {
 
         expect(facebookAuth.execute).toHaveBeenCalledWith({ token: 'any_token' })
         expect(facebookAuth.execute).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return 401 if invalid credentials are provided', async () => {
+        facebookAuth.execute.mockResolvedValueOnce(new AuthenticationError())
+
+        const httpResponse = await sut.handle({ token: 'any_token' })
+
+        expect(httpResponse).toEqual({
+            statusCode: 401,
+            data: new AuthenticationError()
+        })
     })
 })
